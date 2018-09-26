3e23d9d81b28bd00317f91362c15bfcf0cbb0f49
Shehu <Shehu@Jonden.tech>
2018-09-26 23:20:30 +0200

    fetch with Json data

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.getElementById('button1').addEventListener('click',getText);
+document.getElementById('button2').addEventListener('click',getJson);
 
+//get local text file data
 function getText() {
   fetch('test.txt')
     .then(function(res){
@@ -9,6 +11,26 @@ function getText() {
       console.log(data);
       let output = document.getElementById('output');
       output.innerHTML = data;
+
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+//Get local json data
+function getJson() {
+  fetch('posts.json')
+    .then(function(res){
+     return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+         output += `<li>${post.title}</li>`; 
+      });
+      document.getElementById('output').innerHTML = output;
 
     })
     .catch(function(err){
