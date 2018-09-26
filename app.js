d9327965a948181af955dd7c1aa07b56db9902b9
Shehu <Shehu@Jonden.tech>
2018-09-26 23:35:55 +0200

    fetch with API data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.getElementById('button1').addEventListener('click',getText);
 document.getElementById('button2').addEventListener('click',getJson);
+document.getElementById('button3').addEventListener('click',getExternal);
 
 //get local text file data
 function getText() {
@@ -29,6 +30,27 @@ function getJson() {
       let output = '';
       data.forEach(function(post) {
          output += `<li>${post.title}</li>`; 
+      });
+      document.getElementById('output').innerHTML = output;
+
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+
+//Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res){
+     return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+         output += `<li>${user.login}</li>`; 
       });
       document.getElementById('output').innerHTML = output;
 
